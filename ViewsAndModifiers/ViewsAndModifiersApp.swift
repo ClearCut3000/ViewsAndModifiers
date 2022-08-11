19c73821a428f9de253def3e2903d5080379445b
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-12 00:16:22 +0300

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Николай Никитин on 12.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
